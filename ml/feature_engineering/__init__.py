5ec1caa7f8ef2ba9be6b7ff9a3a3119d40e39efe
keerthivasan91 <keerthivasan1617@gmail.com>
2026-03-30 10:41:29 +0530

    fix: Update import statements in feature_engineering and layer2a modules for consistency

--- ml/feature_engineering/__init__.py
+++ ml/feature_engineering/__init__.py
@@ -1,7 +1,9 @@
 """ml/feature_engineering/__init__.py"""
-from .extractor import extract_features, to_vector, extract_vector, FEATURE_NAMES # type: ignore
-from .tokenizer import CharTokenizer, VOCAB_SIZE # type: ignore
-from .normalizer import Normalizer # type: ignore
+
+# Change from .extractor to ml.feature_engineering.extractor
+from ml.feature_engineering.extractor import extract_features, to_vector, extract_vector, FEATURE_NAMES # type: ignore
+from ml.feature_engineering.tokenizer import CharTokenizer, VOCAB_SIZE # type: ignore
+from ml.feature_engineering.normalizer import Normalizer # type: ignore
 
 __all__ = [
     "extract_features", "to_vector", "extract_vector", "FEATURE_NAMES",
--- ml/layer2a/__init__.py
+++ ml/layer2a/__init__.py
@@ -1,3 +1,4 @@
-from .extractor import extract_features, to_vector, extract_vector, FEATURE_NAMES
-from .tokenizer import CharTokenizer, VOCAB_SIZE
-from .normalizer import FeatureNormalizer
+# Change these lines in ml/layer2a/__init__.py
+from feature_engineering.extractor import extract_features, to_vector, extract_vector, FEATURE_NAMES
+from feature_engineering.tokenizer import CharTokenizer, VOCAB_SIZE
+from feature_engineering.normalizer import Normalizer  # Note: your file used 'Normalizer' class
